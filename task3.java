fe4e3bd35bf034ae25c0d4f0e1dc4ae00410b3e5
Kayen <kaitlincyen@gmail.com>
2025-11-17 23:47:14 -0800

    Added task 3 interfacing

---
+++ task3.java
@@ -0,0 +1,56 @@
+// Write a program that is passed: number of sectors, number of tracks, 
+// number of cylinders of a given hard disk and a logical block number 
+// (all in decimals) and have it output the logical block number in 
+// <Cylinder#, plate# , Sector #> format.
+// Note : 0th sector is at <0,0,0> and 1st sector is at <0,0,1> 
+
+// As an example, your program would run as follows:
+// Enter a logical block number:1041
+// Enter HD number of cylinders: 1000
+// Enter HD number of tracks: 20
+// Enter HD number of sectors: 63
+// The logical block number 1041 is located at < 0 , 16 , 33>
+
+import java.util.Scanner;
+
+public class task3 {
+
+    public static int calculatePlateNumber(int blockNum, int numCylinder, int numTracks, int numSectors){
+        return -1;
+    }
+
+    public static int calculateSectorNumber(int blockNum, int numCylinder, int numTracks, int numSectors){
+        return -1;
+    }
+
+    public static int calculateCylinderIndex(int blockNum, int numCylinder, int numTracks, int numSectors){
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        // Initialize Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Take in inputs page size and virtual address for calculation
+        System.out.print("Enter a logical block number: ");
+        int blockNum = scanner.nextInt();
+
+        System.out.print("Enter HD number of cylinders: ");
+        int numCylinder = scanner.nextInt();
+
+        System.out.print("Enter HD number of tracks: ");
+        int numTracks = scanner.nextInt();
+
+        System.out.print("Enter HD number of sectors: ");
+        int numSectors = scanner.nextInt();
+        scanner.close();
+
+        // Do calculations
+        int plateNum = calculatePlateNumber(blockNum, numCylinder, numTracks, numSectors);
+        int sectorNum = calculateSectorNumber(blockNum, numCylinder, numTracks, numSectors);
+        int cylinderIndex = calculateCylinderIndex(blockNum, numCylinder, numTracks, numSectors);
+
+        // Print results
+        System.out.printf("The logical block number %d is located at < %d , %d , %d >\n", blockNum, cylinderIndex, plateNum, sectorNum);
+    }
+}
